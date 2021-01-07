5a7cf8a3f25d454e2ad778d528fa4d08b58e7d6b
Andreas Brett <andreasbrett@users.noreply.github.com>
2021-01-07 08:45:37 +0100

    Update zsh_install.sh

--- zsh_install.sh
+++ zsh_install.sh
@@ -3,6 +3,9 @@
 # go to home folder
 cd $HOME
 
+# install zsh
+sudo apt-get install zsh
+
 # clone oh-my-zsh
 git clone http://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 
@@ -12,6 +15,12 @@ mv ~/.oh-my-zsh $HOME/.local/share/oh-my-zsh
 # install plugins
 sudo apt-get install httpie
 git clone https://github.com/zsh-users/zsh-completions $HOME/.local/share/oh-my-zsh/custom/plugins/zsh-completions
+
+# start zsh
+zsh
+
+# change shell to zsh
+chsh -s /bin/zsh
 
 # relaunch zsh
 source ~/.zshrc
